75c91866066ab8125372b0ec1fb8b9f355b9e079
NaraeJang <j.naaaarae@gmail.com>
2023-03-29 10:19:32 -0400

    Review it again

--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
- 
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
   }
 
-  
-  render() {
-   
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
+      response.json().then((users) => {
+        this.setState(() => {
+          return { monsters: users };
+        });
+      });
+    });
+  }
 
-   
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
-    
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
