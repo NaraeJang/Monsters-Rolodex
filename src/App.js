d76302dbb41b9b8eb6eeb5b4071770c298422f79
NaraeJang <j.naaaarae@gmail.com>
2023-03-29 09:58:39 -0400

    Set up redoAll_v2

--- src/App.js
+++ src/App.js
@@ -2,52 +2,16 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
+ 
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
-      response.json().then((users) => {
-        this.setState(() => {
-          return { monsters: users };
-        });
-      });
-    });
-  }
-
+  
   render() {
-    const filteredMonster = this.state.monsters.filter((monster) => {
-      return monster.name.includes(this.state.searchField);
-    });
+   
 
-    // original
+   
     return (
-      <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value;
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
-        />
-        {filteredMonster.map((monster) => {
-          return (
-            <div id={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
-      </div>
+    
     );
   }
 }
