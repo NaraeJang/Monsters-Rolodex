a2d5534e092125b5ac8881b9cfc4964015392856
NaraeJang <j.naaaarae@gmail.com>
2023-03-28 23:17:13 -0400

    Set up the environment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { Component } from "react";
-
-// import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return <div className="App"></div>;
-  }
+function App() {
+  return <div className="App"></div>;
 }
 
 export default App;
