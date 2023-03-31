b5086bf136a59d4baeb5471ed2fa190e5d649878
NaraeJang <j.naaaarae@gmail.com>
2023-03-30 22:58:54 -0400

    Card component

---
+++ src/components/card-list/card-container.component.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "./card-list.styles.css";
+
+class CardContainer extends Component {
+  render() {
+
+
+    return (
+        );
+  }};
+
+  export default CardContainer;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
     console.log("CardList Log");
+
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => {
           const { id, email, name } = monster;
 
